502175a2087f36f938264eb875446e4e9294f874
Connor Shaughnessy <connor@gamestartschool.org>
2015-03-29 19:34:13 -0400

    Working now, but with terrible performance!  Not sure why this is yet.

--- ServerPythonInterpreterPlugin/src-canary/com/macuyiko/canaryconsole/MainPlugin.java
+++ ServerPythonInterpreterPlugin/src-canary/com/macuyiko/canaryconsole/MainPlugin.java
@@ -2,13 +2,23 @@ package com.macuyiko.canaryconsole;
 
 import java.io.IOException;
 
+import org.gamestart.minecraft.modding.canary.ItemSwingSpellListener;
+import org.gamestart.minecraft.modding.database.IDatabaseBoss;
+import org.gamestart.minecraft.modding.database.MongoDBAccessor;
+import org.gamestart.minecraft.modding.database.MongoDBAccessorFactory;
+import org.gamestart.minecraft.modding.database.factories.DBBossFactory;
+
 import com.macuyiko.minecraftpyserver.ConsolePlugin;
 import com.macuyiko.minecraftpyserver.SetupUtils;
 
+import net.canarymod.Canary;
 import net.canarymod.plugin.Plugin;
 
 public class MainPlugin extends Plugin {
 	
+	public static final MongoDBAccessor TEST_DB_ACCESSOR = MongoDBAccessorFactory.manufacture("localhost", 22715, "techmageserver", "kick2thegroin", "techmageservertest");
+
+
 	public MainPlugin() {
 		super();
 	}
@@ -23,12 +33,22 @@ public class MainPlugin extends Plugin {
     		getConfig().save();
     		SetupUtils.setup();
     		ConsolePlugin.start(this, tcpsocketserverport, websocketserverport, serverpass);
+    		
+        	logEnablingDatabaseConnection();
+            IDatabaseBoss theBoss = DBBossFactory.manufactureMongoDBBoss(TEST_DB_ACCESSOR);
+//        	IDatabaseBoss totallyFakeBossForTesting = new FakeDatabaseBoss();
+            Canary.hooks().registerListener(new ItemSwingSpellListener(theBoss, getLogman()), this);
 		} catch (IOException e) {
 			getLogman().error(e.getMessage());
 			return false;
 		}
     	return true;
     }
+    
+	private void logEnablingDatabaseConnection() {
+		getLogman().info("Enabling database connection for TechMage - version " + getVersion());
+	}
+
 
 	@Override
 	public void disable() {}
--- ServerPythonInterpreterPlugin/src-canary/org/gamestart/minecraft/modding/canary/ItemSwingSpellListener.java
+++ ServerPythonInterpreterPlugin/src-canary/org/gamestart/minecraft/modding/canary/ItemSwingSpellListener.java
@@ -3,28 +3,46 @@ package org.gamestart.minecraft.modding.canary;
 import net.canarymod.api.inventory.Item;
 import net.canarymod.hook.HookHandler;
 import net.canarymod.hook.player.PlayerArmSwingHook;
+import net.canarymod.logger.Logman;
 import net.canarymod.plugin.PluginListener;
 
 import org.gamestart.minecraft.modding.database.IDatabaseBoss;
+import org.python.core.PyObject;
+import org.python.util.InteractiveInterpreter;
+
+import com.macuyiko.canaryconsole.CanaryParser;
+import com.macuyiko.minecraftpyserver.ConsolePlugin;
 
 public class ItemSwingSpellListener implements PluginListener {
 	
 	private IDatabaseBoss theBoss;
+	private Logman logman;
 
-	public ItemSwingSpellListener(IDatabaseBoss theBoss){
+	public ItemSwingSpellListener(IDatabaseBoss theBoss, Logman logman){
 		this.theBoss = theBoss;
+		this.logman = logman;
 	}
 	
     @HookHandler
     public void onSwingItem(PlayerArmSwingHook hook) {
         Item itemHeld = hook.getPlayer().getItemHeld();
-        if(itemHeld == null || itemHeld.getType() == null){
-        }
-        else{
-        	String userSpellForItemFromMongoDB = theBoss.getUserSpellForItem(itemHeld.getType(), hook.getPlayer().getDisplayName());
+        String playerName = hook.getPlayer().getDisplayName();
+        String userSpellForItemFromMongoDB = theBoss.getUserSpellForItem(itemHeld.getType(), playerName);
+        if(itemHeld != null && itemHeld.getType() != null){
         	if(userSpellForItemFromMongoDB != null && userSpellForItemFromMongoDB != ""){
-        		//DO THE PYTHON HERE, HOWEVER SEPPE DOES THE PYTHON!
+        		executePythonFromPlayer(userSpellForItemFromMongoDB, playerName);
         	}       
         }
     }
+
+	private void executePythonFromPlayer(String python, String playerName) {
+		try{
+		InteractiveInterpreter interpreter = new InteractiveInterpreter(
+				null, ConsolePlugin.getPythonSystemState());
+		logman.info("Executing python on behalf of " + playerName + ": " + python);
+		CanaryParser.parse(interpreter, "from mcapi import *\n"+"player = getplayer('"+playerName+"')\n"+python,true);
+		}catch(Exception e){
+			logman.catching(e);
+		}
+	}
 }
